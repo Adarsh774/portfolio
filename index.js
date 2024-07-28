61051a165e02a57db7baba02e423ff38adaba32a
Adarsh774 <co21adarshyadav@gmail.com>
2024-07-28 15:24:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+// function darkMode() {
+//     let container = document.getElementById('about-container')
+//     container.style.boxShadow = 'box-shadow: 0 10px 20px rgba(255, 255, 255, 0.415);'
+//     var element = document.body;
+//     let h3 = document.getElementById('logo')
+//     let li = document.getElementById('li')
+//     let home = document.getElementById('home')
+//     let nav = document.getElementById('nav-bar')
+//     nav.style.background = '#17153B'
+//     nav.style.boxShadow = '0 10px 20px 8px rgb(255 255 255 / 10%)'
+//     // console.log(li);
+//     home.style.background = '#17153B'
+//     element.classList.toggle("dark");
+//     h3.style.color = 'white'
+//     li.style.color = 'white'
+//     // box-shadow: 0 10px 20px rgba(255, 255, 255, 0.415);
+// }
+// darkMode()
