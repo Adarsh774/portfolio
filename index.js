7c427ec5701526f82963f4f9d3c021c0a780e462
sonu <b070138.blossomms@gmail.com>
2024-08-04 13:22:23 +0530

    add api of contact form

--- index.js
+++ index.js
@@ -1,22 +1,4 @@
-// function darkMode() {
-//     let container = document.getElementById('about-container')
-//     container.style.boxShadow = 'box-shadow: 0 10px 20px rgba(255, 255, 255, 0.415);'
-//     var element = document.body;
-//     let h3 = document.getElementById('logo')
-//     let li = document.getElementById('li')
-//     let home = document.getElementById('home')
-//     let nav = document.getElementById('nav-bar')
-//     nav.style.background = '#17153B'
-//     nav.style.boxShadow = '0 10px 20px 8px rgb(255 255 255 / 10%)'
-//     // console.log(li);
-//     home.style.background = '#17153B'
-//     element.classList.toggle("dark");
-//     h3.style.color = 'white'
-//     li.style.color = 'white'
-//     // box-shadow: 0 10px 20px rgba(255, 255, 255, 0.415);
-// }
-// darkMode()
-
+// function for send email
 function emailsend() {
     var params = {
         name: document.getElementById("name").value,
@@ -25,12 +7,17 @@ function emailsend() {
     }
     const serviceID = "service_uyl5ggb";
     const templateID = "template_aphkrhl"
-    
+
     emailjs.send(serviceID, templateID, params)
-    .then((res) => {
-        document.getElementById("name").value="";
-        document.getElementById("email").value="";
-        document.getElementById("message").value="";
-        alert("your message sent")
-    }).catch((err)=> console.log(err));
+        .then((res) => {
+            document.getElementById("name").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("message").value = "";
+            alert("your message sent")
+        }).catch((err) => console.log(err));
+}
+// function for load about page 
+function about() {
+    window.location.href(
+        "about.html");
 }
